2f26461eb2335bd8e5f2e0e62e3f5cb36b548d0e
shaomingfa <lanseyoukong638@163.com>
2021-01-21 15:34:13 +0800

    加入kotlin代码

---
+++ app/src/main/java/com/convert/mymp3convert/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.convert.mymp3convert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
